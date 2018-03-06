45a76d379ad351aa7ddc5bb7394906a1af87cb0a
cybaer <s.k.g@gmx.de>
2018-03-06 08:33:10 +0100

    HardwareConfig.h added

---
+++ HardwareConfig.h
@@ -0,0 +1,81 @@
+/*
+ * HardwareConfig.h
+ *
+ *  Created on: 09.02.2017
+ *      Author: cybaer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef HARDWARECONFIG_H_
+#define HARDWARECONFIG_H_
+
+#include "lib/ui/EdgeTrigger.h"
+#include "lib/ui/analogSwitch.h"
+#include "avrlib/base.h"
+#include "avrlib/gpio.h"
+#include "avrlib/adc.h"
+
+
+using namespace avrlib;
+
+typedef Inverter<Gpio<PortD, 7> > Trigger;  // 13
+typedef EdgeTrigger<Gpio<PortB, 2>, 0> ResetIn;  // 16
+
+typedef Inverter<Gpio<PortD, 6> > Output_1;  // 6
+typedef Inverter<Gpio<PortD, 4> > Output_2;  // 4
+typedef Inverter<Gpio<PortD, 3> > Output_3;  // 3
+typedef Inverter<Gpio<PortD, 2> > Output_4;  // 2
+
+typedef Inverter<Gpio<PortB, 6> > OutputReturn;  // 9
+
+typedef AnalogSwitch<Adc, 0, 4> SwitchSteps;
+
+extern Adc adc;
+
+inline void initInputs(void)
+{
+  ResetIn::init();
+  Trigger::set_mode(DIGITAL_INPUT);
+  Trigger::High();
+}
+inline void initOutputs(void)
+{
+  Output_1::set_mode(DIGITAL_OUTPUT);
+  Output_1::set_value(true);
+  Output_2::set_mode(DIGITAL_OUTPUT);
+  Output_2::set_value(true);
+  Output_3::set_mode(DIGITAL_OUTPUT);
+  Output_3::set_value(true);
+  Output_4::set_mode(DIGITAL_OUTPUT);
+  Output_4::set_value(true);
+  OutputReturn::set_mode(DIGITAL_OUTPUT);
+  OutputReturn::set_value(true);
+}
+
+inline void initAnalogIn(void)
+{
+  adc.Init();
+}
+
+inline void initHW(void)
+{
+  initInputs();
+  initOutputs();
+  initAnalogIn();
+}
+
+
+
+
+#endif /* HARDWARECONFIG_H_ */
